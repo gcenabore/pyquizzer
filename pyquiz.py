a80880576069edf07df86e2b82faafdb396d2492
unknown <gianenabore30@gmail.com>
2025-01-26 15:00:06 +0800

    list of questions

--- pyquiz.py
+++ pyquiz.py
@@ -6,4 +6,35 @@
 #store the user's info and score in a txt file, to find their data later
 #display the user's score
 
+questions = [
+    { 
+        "question": "Who is the creator of python?",
+        "options": ["A. Dennis Ritchie", "B. Guido Van Rossum", "C. James Gosling", "D. Brendan Eich"],
+        "answer": "B"
+    },
 
+    {
+        "question": "In which year was python released?",
+        "options": ["A. 1991", "B. 1989", "C. 1995", "D. 2000"],
+        "answer": "A"
+    },
+
+    {
+        "question": "The name 'python' was derived from which of the following?",
+        "options": ["A. A type of snake", "B. A british comedy troupe", "C. A scientific term", "D. An acronym"],
+        "answer": "B"
+    },
+
+    {
+        "question": "Which of the following is NOT a key philosophy of Python as stated in 'The Zen of Python'?",
+        "options": ["A. Readability counts", "B. Complex is better than simple", "C. There should be one—and preferably only one—obvious way to do it", "D. Speed is the most important feature"],
+        "answer": "D"
+    },
+
+    {
+        "question": "Python 2 was officially discontinued in which year?",
+        "options": ["A. 2020", "B. 2019", "C. 2021", "D. 2022"],
+        "answer": "A"
+    }
+
+]
