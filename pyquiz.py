5d9409dd8c9e29e02d92f2d085676fd344b1179e
unknown <gianenabore30@gmail.com>
2025-01-26 14:49:18 +0800

    initial commit, pseudocode

---
+++ pyquiz.py
@@ -0,0 +1,9 @@
+#create a list of questions
+#ask the user's info before answering the questions
+#set conditions for the answers
+#ask the user for answer, if correct, print correct, otherwise print incorrect
+#count the number of correct answers
+#store the user's info and score in a txt file, to find their data later
+#display the user's score
+
+
